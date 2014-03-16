fe7dfa93299af97f294bc5367c0535f8ba021863
lemocen <zhfayu1989@hotmal.com>
2014-03-16 14:59:38 +0800

    repository created

---
+++ Sort/bubblesourt.c
@@ -0,0 +1,16 @@
+/*
+ * bubblesourt.c - 
+ *
+ *
+ * Author: lemocen (zhfayu1989@hotmil.com)
+ * Create: 2014-03-16 14:40:14
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
+
---
+++ Sort/quicksort.c
@@ -0,0 +1,16 @@
+/*
+ * quicksort.c - 
+ *
+ *
+ * Author: lemocen (zhfayu1989.hotmail.com)
+ * Create: 2014-03-16 14:39:04
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
+
